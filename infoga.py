38e21cc82eaa613f4ae2ab44f19738754a5d86fc
F4HAD0_0 <147190499+0xF4HAD@users.noreply.github.com>
2023-10-07 23:33:25 +0600

    Update infoga.py

--- infoga.py
+++ infoga.py
@@ -5,91 +5,116 @@ import socket
 import argparse
 import requests
 from bs4 import BeautifulSoup
+from colorama import init,Fore
+import pyfiglet
 
+
+init()
+green =Fore.GREEN
+greenx =Fore.LIGHTGREEN_EX
+yellow =Fore.YELLOW
+red = Fore.RED
+blue=Fore.LIGHTBLUE_EX
+reset =Fore.RESET
+
+
+banner = pyfiglet.figlet_format('inf0g4')
+print(f'{blue}')
+print(banner)
+print(f'{reset}')
+print(f'{blue}Script{reset}: {red}0xF4HAD{reset}')
+
+print(f'{green}={reset}' * 50)
+print('         DNS Information Gathering Tools        ')
+print(f'{green}={reset}' * 50)
 
 def get_whois_info(domain):
     try:
-        print('[++] Getting Whois info...')
-        print('=' * 50)
+        print(f'\n{greenx}[++] Getting Whois info...')
+        print(f'{green}={reset}' * 50)
         w = whois.query(domain)
 
-        print('[*] Name: {}'.format(w.name))
-        print('[*] Registrar: {}'.format(w.registrar))
-        print('[*] Creation Date: {}'.format(w.creation_date))
-        print('[*] Expiration Date: {}'.format(w.expiration_date))
-        print('[*] Name Servers: {}'.format(w.name_servers))
-        print('[*] Registrant: {}'.format(w.registrant))
+        # print(f'{yellow}[*] Name: {}'.format(w.name))
+        print(f'{yellow}[*]{reset} Name: {w.name}')
+        print(f'{yellow}[*]{reset} Registrar: {w.registrar}')
+        print(f'{yellow}[*]{reset} Creation Date: {w.creation_date}')
+        print(f'{yellow}[*]{reset} Expiration Date: {w.expiration_date}')
+        print(f'{yellow}[*]{reset} Name Servers: {w.name_servers}')
+        print(f'{yellow}[*]{reset} Registrant: {w.registrant}')
     except Exception as e:
-        print('[-] Error getting Whois info: {}'.format(e))
+        print(f'{red}[*]{reset} Error getting Whois info: {e}')
 
 
 def get_dns_info(domain):
     try:
-        print('\n[++] Getting DNS info...')
-        print('=' * 50)
+        
+        print(f'\n{greenx}[++] Getting DNS info...')
+        print(f'{green}={reset}' * 50)
 
         def print_records(records, record_type):
             for record in records:
-                print('[*] {} Record: {}'.format(record_type, record.to_text()))
+                print(f'{yellow}[*]{reset} {record_type} Record: {record.to_text()}')
 
         for record_type in ['A', 'AAAA', 'CNAME', 'MX', 'NS', 'TXT']:
             try:
                 records = dns.resolver.resolve(domain, record_type)
                 print_records(records, record_type)
             except Exception as e:
-                print('[-] {} Record could not resolve'.format(record_type))
+                print(f'{red}[-]{reset} {record_type} Record could not resolve')
 
     except Exception as e:
-        print('[-] Error getting DNS info: {}'.format(e))
+        print(f'{red}[-]{reset} Error getting DNS info: {e}')
 
-
+       
 # Function to get DNS info including reverse DNS lookup
 
 def get_reversedns_lookup(domain):
     try:
-        print('\n[++] Reverse DNS Lookup...')
-        print('=' * 50)
+        print(f'\n{greenx}[++] Reverse DNS Lookup...')
+        print(f'{green}={reset}' * 50)
         ip = socket.gethostbyname(domain)
         if ip:
-            print('[*] IP Address: {}'.format(ip))
+            print(f'{yellow}[*]{reset}IP Address: {ip}')
 
             # Reverse DNS lookup
             try:
                 hostnames = socket.gethostbyaddr(ip)
-                print('[*] Reverse DNS Lookup:')
+                print(f'{yellow}[*]{reset} Reverse DNS Lookup:')
                 for hostname in hostnames:
                     print('   - {}'.format(hostname))
             except Exception as e:
-                print('[-] Reverse DNS lookup failed: {}'.format(e))
+                print(f'{yellow}[-]{reset} Reverse DNS lookup failed: {e}')
     except Exception as e:
-        print('[-] Error getting DNS info: {}'.format(e))
+        print(f'{yellow}[-]{reset} Error getting DNS info: {e}')
 
 
 def get_geolocation_info(domain):
     try:
-        print('\n[++] Getting Geolocation info...')
-        print('=' * 50)
+        
+        print(f'\n{greenx}[++] Getting Geolocation info...')
+        print(f'{green}={reset}' * 50)
         ip = socket.gethostbyname(domain)
         if ip:
             response = requests.get(
                 'http://geolocation-db.com/json/' + ip).json()
-            print('[*] Country Code: {}'.format(response['country_code']))
-            print('[*] Country Name: {}'.format(response['country_name']))
-            print('[*] City: {}'.format(response['city']))
-            print('[*] Postal: {}'.format(response['postal']))
-            print('[*] Latitude: {}'.format(response['latitude']))
-            print('[*] Longitude: {}'.format(response['longitude']))
-            print('[*] IPv4: {}'.format(response['IPv4']))
-            print('[*] State: {}'.format(response['state']))
+            print('{}[*]{} Country Code: {}'.format(yellow,reset,response['country_code']))
+            print('{}[*]{} Country Name: {}'.format(yellow,reset,response['country_name']))
+            print('{}[*]{} City: {}'.format(yellow,reset,response['city']))
+            print('{}[*]{} Postal: {}'.format(yellow,reset,response['postal']))
+            print('{}[*]{} Latitude: {}'.format(yellow,reset,response['latitude']))
+            print('{}[*]{} Longitude: {}'.format(yellow,reset,response['longitude']))
+            print('{}[*]{} IPv4: {}'.format(yellow,reset,response['IPv4']))
+            print('{}[*]{} State: {}'.format(yellow,reset,response['state']))
 
     except Exception as e:
-        print('[-] Error getting Geolocation info: {}'.format(e))
+        print('{}[-]{} Error getting Geolocation info: {}'.format(red,reset,e))
 
 
 def get_shodan_info(domain):
     try:
-        print('\n[++] Getting Shodan info...')
-        print('=' * 50)
+        
+        print(f'\n{greenx}[++] Getting Shodan info...')
+        print(f'{green}={reset}' * 50)
         ip = socket.gethostbyname(domain)
         if ip:
             api = shodan.Shodan("YourShodanApiKey")
@@ -106,8 +131,8 @@ def get_shodan_info(domain):
 # Function to extract email addresses from a webpage
 def extract_emails(url):
     try:
-        print('[++] Extracting Email Addresses...')
-        print('=' * 50)
+        print(f'\n{greenx}[++] Extracting Email Addresses...')
+        print(f'{green}={reset}' * 50)
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         emails = set()
@@ -116,11 +141,15 @@ def extract_emails(url):
                 emails.add(email['href'].replace('mailto:', ''))
         if emails:
             for email in emails:
-                print('[*] Email: {}'.format(email))
+                print('{}[*]{} Email: {}'.format(yellow,reset,email))
         else:
-            print('[-] No email addresses found on the webpage.')
+            print(f'{red}[-]{yellow} No email addresses found on the webpage.')
     except Exception as e:
-        print('[-] Error extracting email addresses: {}'.format(e))
+        print('{}[-]{} Error extracting email addresses: {}'.format(red,reset,e))
+
+
+
+
 
 
 # Function to get website title
@@ -128,14 +157,14 @@ def extract_emails(url):
 
 def get_website_title(url):
     try:
-        print('\n[++] Getting Website Title...')
-        print('=' * 50)
+        print(f'\n{greenx}[++] Getting Website Title...')
+        print(f'{green}={reset}' * 50)
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         title = soup.title.string.strip() if soup.title else 'N/A'
-        print('[*] Website Title: {}'.format(title))
+        print('{}[*]{} Website Title: {}'.format(yellow,reset,title))
     except Exception as e:
-        print('[-] Error getting website title: {}'.format(e))
+        print('{}[-]{} Error getting website title: {}'.format(red,reset,e))
 
 
 if __name__ == '__main__':
